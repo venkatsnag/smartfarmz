4b2e6d722415942ebe7763996bf6588d350720b7
venkat <venkatsnag@gmail.com>
2021-01-03 15:39:23 +0100

    Prod

--- lib/providers/apiClass.dart
+++ lib/providers/apiClass.dart
@@ -2,12 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppApi {
 
-<<<<<<< Updated upstream
  static var api ='http://api.happyfarming.net:5000';
    //static var api ='http://192.168.68.100:5000';
-=======
- //static var api ='http://api.happyfarming.net:5000';
-  static var api ='http://192.168.68.100:5000';
-   // static var api ='http://192.168.68.100:5000';
->>>>>>> Stashed changes
 }
