661b802d85f23ace237415cf2c9c7048393353fd
venkat <venkatsnag@gmail.com>
2021-01-03 15:34:14 +0100

    new api

--- lib/providers/apiClass.dart
+++ lib/providers/apiClass.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 class AppApi {
 
  //static var api ='http://api.happyfarming.net:5000';
+   //static var api ='http://192.168.68.100:5000';
    static var api ='http://192.168.68.100:5000';
 }
